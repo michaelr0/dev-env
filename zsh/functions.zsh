2b8be07e366555c68c9164a12bc442e3c2965ee4
Michael Rook <michael@rook.net.au>
2022-08-08 14:16:08 +1000

    Add additional function helpers for phpunit and laravel sail

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -12,3 +12,35 @@ function phpfmt() {
         echo "PHP CS Fixer not found!"
     fi
 }
+
+# PHPUNIT
+function phpunit() {
+    if [ -x "vendor/bin/phpunit" ]; then
+        EXEC_BIN="vendor/bin/phpunit"
+    else
+        EXEC_BIN="$(which phpunit)"
+    fi
+
+    if [ -x $EXEC_BIN ]; then
+        $EXEC_BIN $@
+    else
+        echo "PHPUnit not found!"
+    fi
+}
+
+# Laravel Sail
+function sail() {
+    if [ -x "sail" ]; then
+        EXEC_BIN="sail"
+    elif [ -f "vendor/bin/sail" ]; then
+        EXEC_BIN="vendor/bin/sail"
+    else
+        EXEC_BIN="$(which sail)"
+    fi
+
+    if [ -x $EXEC_BIN ]; then
+        $EXEC_BIN $@
+    else
+        echo "Laravel Sail not found!"
+    fi
+}
