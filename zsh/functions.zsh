9334d6f6564e2b920a0a4907308c753d3e73dcdb
Michael Rook <michael@rook.net.au>
2022-08-08 14:15:46 +1000

    Expand logic

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,4 +1,14 @@
 # php-cs-fixer fix command with config automatically applied
 function phpfmt() {
-    php-cs-fixer fix --config="$HOME/dev-env/php-cs-fixer.dist.php" $@
+    if [ -x "vendor/bin/php-cs-fixer" ]; then
+        EXEC_BIN="vendor/bin/php-cs-fixer"
+    else
+        EXEC_BIN="$(which php-cs-fixer)"
+    fi
+
+    if [ -x $EXEC_BIN ]; then
+        $EXEC_BIN fix --config="$HOME/dev-env/php-cs-fixer.dist.php" $@
+    else
+        echo "PHP CS Fixer not found!"
+    fi
 }
