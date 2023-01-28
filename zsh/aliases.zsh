6fffddd05eccfbc1749def43ee5143a65cbfdebb
Michael Rook <michael@rook.net.au>
2023-01-29 08:47:12 +1100

    Move php alias into aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,19 @@ alias ls="ls --color=auto"
 alias grep='grep --color=auto'
 alias path='echo -e ${PATH//:/\\n}'
 
+PHP_VERSIONS=(7.4 8.0 8.1 8.2)
+
+for PHP_VER in $PHP_VERSIONS
+do
+    if [ -x "${HOMEBREW_PREFIX}/opt/php@${PHP_VER}/bin/php" ]; then
+        alias "php${PHP_VER//./}"="${HOMEBREW_PREFIX}/opt/php@${PHP_VER}/bin/php"
+
+        if [ -x "${HOMEBREW_PREFIX}/bin/composer" ]; then
+            alias "composer${PHP_VER//./}"="php${PHP_VER//./} ${HOMEBREW_PREFIX}/bin/composer"
+        fi
+    fi
+done
+
 # Aliases
 if [ -f "$HOME/.aliases.zsh" ]; then
     source "$HOME/.aliases.zsh"
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -50,19 +50,6 @@ function sail() {
     fi
 }
 
-PHP_VERSIONS=(7.4 8.0 8.1 8.2)
-
-for PHP_VER in $PHP_VERSIONS
-do
-    if [ -x "${HOMEBREW_PREFIX}/opt/php@${PHP_VER}/bin/php" ]; then
-        alias "php${PHP_VER//./}"="${HOMEBREW_PREFIX}/opt/php@${PHP_VER}/bin/php"
-
-        if [ -x "${HOMEBREW_PREFIX}/bin/composer" ]; then
-            alias "composer${PHP_VER//./}"="php${PHP_VER//./} ${HOMEBREW_PREFIX}/bin/composer"
-        fi
-    fi
-done
-
 # Functions
 if [ -f "$HOME/.functions.zsh" ]; then
     source "$HOME/.functions.zsh"
